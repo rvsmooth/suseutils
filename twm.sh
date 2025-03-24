1b6f46d1e6ff9bb28423fce558ccd5fb5ade1838
rvsmooth <riveeks.smooth@gmail.com>
2025-03-24 07:38:28 +0530

    chore: Add hyprland packages

--- twm.sh
+++ twm.sh
@@ -1,20 +1,34 @@
 #!/usr/bin/env bash
 
-source utils.sh 
+source utils.sh
 
 wm_pkgs=(
-	"feh"
-	"flameshot"
-	"picom"
-	"python313-psutil"
-	"qtile"
-	"redshift"
-	"sddm"
-	"xclip"
-	"xorg-x11-essentials"
-	"xorg-x11-server"
+  "feh"
+  "flameshot"
+  "picom"
+  "python313-psutil"
+  "qtile"
+  "redshift"
+  "sddm"
+  "xclip"
+  "xorg-x11-essentials"
+  "xorg-x11-server"
 )
 
+wm_pkgs_hypr=(
+  "gegl"
+  "hyprcursor"
+  "hypridle"
+  "hyprland"
+  "hyprland-qtutils"
+  "hyprland-wallpapers"
+  "libdrm-tools"
+  "libhyprutils4"
+  "libpng16-16"
+  "libxkbcommon0"
+  "libxml2-tools"
+  "llvm"
+)
 
 PYELL Installing window manager packages
 
