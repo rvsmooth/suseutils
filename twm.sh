f89f7c5e90b37c36eb9e20f2a0a9eef14854e3b6
rvsmooth <riveeks.smooth@gmail.com>
2025-04-21 10:52:20 +0530

    chore(twm.sh): Use multi-string variable for better execution

--- twm.sh
+++ twm.sh
@@ -2,36 +2,40 @@
 
 source utils.sh
 
-wm_pkgs=(
-  "feh"
-  "flameshot"
-  "picom"
-  "python313-psutil"
-  "qtile"
-  "redshift"
-  "sddm"
-  "xclip"
-  "xorg-x11-essentials"
-  "xorg-x11-server"
-)
+wm_pkgs="
+  feh
+  flameshot
+  picom
+  python313-psutil
+  qtile
+  redshift
+  sddm
+  xclip
+  xorg-x11-essentials
+  xorg-x11-server
+"
 
-wm_pkgs_hypr=(
-  "gegl"
-  "hyprcursor"
-  "hypridle"
-  "hyprland"
-  "hyprland-qtutils"
-  "hyprland-wallpapers"
-  "libdrm-tools"
-  "libhyprutils4"
-  "libpng16-16"
-  "libxkbcommon0"
-  "libxml2-tools"
-  "llvm"
-)
+wm_pkgs_hypr="
+  dunst
+  gegl
+  grim
+  hyprcursor
+  hypridle
+  hyprland
+  hyprland-qtutils
+  hyprland-wallpapers
+  libdrm-tools
+  libhyprutils4
+  libpng16-16
+  libxkbcommon0
+  libxml2-tools
+  llvm
+  slurp
+"
 
 PYELL Installing window manager packages
-
-__zypper_install "${wm_pkgs[@]}"
+for package in "$wm_pkgs_hypr"; do
+  __zypper_install $package
+done
 
 PDONE
