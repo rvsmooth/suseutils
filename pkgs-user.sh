1c1dd1346030d97e154349c93f072eee8570a776
rvsmooth <riveeks.smooth@gmail.com>
2025-04-21 10:49:06 +0530

    chore: Install vivaldi instead of librewolf

--- pkgs-user.sh
+++ pkgs-user.sh
@@ -1,19 +1,19 @@
-#!/usr/bin/env bash 
+#!/usr/bin/env bash
 
 source utils.sh
 
 user_pkgs=(
-	"btop"
-	"calibre"
-	"kitty"
-	"libreoffice"
-	"mpv"
-	"neovim"
-	"nwg-look"
-	"pcmanfm"
-	"qbittorrent"
-	"rofi"
-	"viewnior"
+  "btop"
+  "calibre"
+  "kitty"
+  "libreoffice"
+  "mpv"
+  "neovim"
+  "nwg-look"
+  "pcmanfm"
+  "qbittorrent"
+  "rofi"
+  "viewnior"
 )
 
 PYELL Installing user packages
@@ -22,17 +22,8 @@ __zypper_install "${user_pkgs[@]}"
 
 PDONE
 
-PYELL Installing librewolf 
-
-if command -v librewolf >/dev/null 2>&1; then 
-	PYELL Librewolf is already installed
-else
-	PYELL Librewolf is not installed 
-	PYELL Installing librewolf 
-
-	sudo rpm --import https://rpm.librewolf.net/pubkey.gpg
-	sudo zypper ar -ef https://rpm.librewolf.net librewolf
-	sudo zypper ref
-	__zypper_install librewolf
-
+PYELL Installing vivaldi
+sudo zypper ar https://repo.vivaldi.com/archive/vivaldi-suse.repo
+sudo zypper ref
+__zypper_install vivaldi-stable
 PDONE
